05daad79d20100e7ea34c78c5ee175ec56ef5426
Ricardo <ricardo.remond.pro@gmail.com>
2024-01-15 15:56:03 +0100

    connex database OP

--- controllers/controller-signup.php
+++ controllers/controller-signup.php
@@ -1,4 +1,7 @@
 <?php
+
+$regexName = "/^[a-zA-ZÀ-ÿ\-]+$/";
+$regexPseudo = "/^[a-zA-ZÀ-ÿ\_\-\d]+$/";
 // Vérification des données postées depuis le formulaire
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $errors = [];
@@ -6,15 +9,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Vérification du nom
     if (empty($_POST["nom"])) {
         $errors['nom'] = "Champs obligatoire.";
-    } else if (!preg_match("/^[a-zA-ZÀ-ÿ\-]+$/", $_POST["nom"])) {
+    } else if (!preg_match($regexName, $_POST["nom"])) {
         $errors['nom'] = "Le nom est invalide.";
     }
 
     // Vérification du prénom
     if (empty($_POST["prenom"])) {
         $errors['prenom'] = "Champs obligatoire.";
-    } else if (!preg_match("/^[a-zA-ZÀ-ÿ\-]+$/", $_POST["nom"])) {
+    } else if (!preg_match($regexName, $_POST["prenom"])) {
         $errors['prenom'] = "Le nom est invalide.";
+    }
+
+    // Vérification du pseudo
+    if (empty($_POST["pseudo"])) {
+        $errors['pseudo'] = "Champs obligatoire.";
+    } else if (!preg_match($regexPseudo, $_POST["pseudo"])) {
+        $errors['pseudo'] = "Le nom est invalide.";
     }
 
     // Vérification de l'email
@@ -36,34 +46,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $errors['password'] = "Le mot de passe doit comporter au moins 8 caractères et correspondre.";
     }
 
+    // Vérification du select
+    if (!isset($_POST["company"])) {
+        $errors['company'] = "Selectionnez une entreprise";
+    }
+
+    // Vérification du CGU
+    if (!isset($_POST["cgu"])) {
+        $errors['cgu'] = "Veuillez valider les CGU";
+    }
+
+
+    // si il n'y a pas d'erreurs 
+
+    var_dump($errors);
     // Affichage des erreurs
     // Si aucune erreur détectée
     if (empty($errors)) {
-        // Afficher la synthèse des informations et le message de confirmation
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $email = $_POST['email'];
-        $dob = $_POST['dob'];
+        try {
+            $dbName = "jeu de donnees";
+            $dbUser = "Rickson";
+            $dbPassword = "Ricardo27";
+            // Creation d'un objet $db selon la class PDO
+            // Connexion a la bdd
+            $db = new PDO("mysql:host=localhost;dbname=$dbName", $dbUser, $dbPassword);
+            var_dump($db);
 
-        // Cacher le formulaire
-        $formHidden = true;
+            // Stockage de ma requete dans une variable 
+            $sql = "INSERT INTO `utilisateur`( `Pseudo`, `Nom`, `Prenom`, `Date_de_naissance`, `Email`, `Mot_de_passe_utilisateur`,`Utilisateur_valide`, `ID_entreprise`) VALUES (:Pseudo, :Nom, :Prenom, :Date_de_naissance, :Email, :Mot_de_passe_utilisateur, :Utilisateur_valide, :ID_Entreprise )";
 
-        // Envoyer le mail de confirmation (simulation ici)
-        $message = "Bonjour $prenom $nom, votre inscription a bien été enregistrée.";
-        // Envoi du mail (à remplacer par votre code d'envoi de mail)
-        // mail($email, 'Confirmation d\'inscription', $message);
+            // Je prepare ma requete pour eviter les injection SQL 
 
-        // Afficher la synthèse des informations et le message de confirmation
-        include('../views/view-summary.php');
-        exit; // Arrêter l'exécution du script
+            $query = $db->prepare($sql);
+            $pseudo = htmlspecialchars($_POST["pseudo"]);
+            $nom = htmlspecialchars($_POST["nom"]);
+            $prenom = htmlspecialchars($_POST["prenom"]);
+            $dob = $_POST["dob"];
+            $email = $_POST["email"];
+            $mdpUtilisateur = password_hash($_POST["password"], PASSWORD_DEFAULT);
+            $idEntreprise = $_POST["company"];
+
+
+            $query->bindValue(":Pseudo", $pseudo, PDO::PARAM_STR);
+            $query->bindValue(":Nom", $nom, PDO::PARAM_STR);
+            $query->bindValue(":Prenom", $prenom, PDO::PARAM_STR);
+            $query->bindValue(":Date_de_naissance", $dob, PDO::PARAM_STR);
+            $query->bindValue(":Email", $email, PDO::PARAM_STR);
+            $query->bindValue(":Mot_de_passe_utilisateur", $mdpUtilisateur, PDO::PARAM_STR);
+            $query->bindValue(":Utilisateur_valide", 1, PDO::PARAM_STR);
+            $query->bindValue(":ID_Entreprise", $idEntreprise, PDO::PARAM_STR);
+
+            $query = $query->execute();
+
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+            die();
+
+        }
     }
 }
 ?>
 <?php
-// Contrôleur - Gestion de la logique métier
-
-// Vérifications et traitements du formulaire ici
-
-// Inclusion de la vue
 include_once('../views/view-signup.php');
 ?>
