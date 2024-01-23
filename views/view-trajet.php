3148f2b3de5634b1b00b1d36309a817aef0869fe
Ricardo <ricardo.remond.pro@gmail.com>
2024-01-23 10:10:29 +0100

    commit

--- views/view-trajet.php
+++ views/view-trajet.php
@@ -21,6 +21,32 @@
             <label for="dureeTrajet">Durée du trajet:</label>
             <input type="time" id="dureeTrajet" name="dureeTrajet" required>
 
+            <label for="idVehicule">Vehicule</label>
+            <select id="company" name="company" required>
+                <option value="" disabled selected>Veuillez sélectionner une entreprise</option>
+                <option value="1" <?php if (!empty($idVehicule) && $idVehicule == "Velo") {
+                    echo "Velo";
+                } ?>> Vélo
+                </option>
+                <option value="2" <?php if (!empty($idVehicule) && $idVehicule == "Trottinette ") {
+                    echo "Trottinette";
+                } ?>>Trottinette</option>
+                <option value="3" <?php if (!empty($idVehicule) && $idVehicule == "Marche") {
+                    echo "Marche";
+                } ?>>Marche</option>
+                <option value="4" <?php if (!empty($idVehicule) && $idVehicule == "Rollers ") {
+                    echo "Rollers";
+                } ?>>Rollers</option>
+                <option value="5" <?php if (!empty($idVehicule) && $idVehicule == "Skate ") {
+                    echo "Skate";
+                } ?>>Skate</option>
+            </select>
+            <span class="error">
+                <?php if (isset($errors['idVehicule'])) {
+                    echo $errors['idVehicule'];
+                } ?>
+            </span>
+
             <label for="imageTrajet">Image du trajet (optionnel):</label>
             <input type="file" id="imageTrajet" name="imageTrajet" accept="image/*">
 
